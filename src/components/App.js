42286e95c0fe3d208545a003bb48690d2e5f83b1
anilvivekk <avanilvivek056@gmail.com>
2022-07-30 22:30:18 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,23 @@ import '../styles/App.css';
 const App = () => {
   
   return (
-
+    <div>
+      <form>
+      <input id='name' placeholder='username' type='text' />
+      <label for='name'>Username</label>
+      <br/>
+      <input id='email' placeholder='email' type='email' />
+      <label for='email'>Email</label>
+      <br />
+      <input id='password' placeholder='password' type='password' />
+      <label for='password'>Password</label>
+      <br />
+      <input id='consent' type='checkbox' />
+      <label for='consent'>Consent</label>
+      <br />
+      <button>Signup</button>
+      </form>
+    </div>
   )
 }
 
